e11f79c3cda212d891e6fbcd188c2203305eefd0
Ross <silberquitr@gmail.com>
2017-04-16 09:27:03 -0400

    Remove initial node stuff

--- app/index.js
+++ app/index.js
@@ -1,18 +1,6 @@
 // app/index.js
 
-const calc = require('./calc')
-
 const guidebox = require('./guidebox')
-
-/*const numbersToAdd = [
-  3,
-  4,
-  10,
-  2
-]*/
-
-/*const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)*/
 
 
 const result = guidebox.shows
