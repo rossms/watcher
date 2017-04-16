7bddb79d2b19057ec6d0b97984876fc120e0771f
Ross <silberquitr@gmail.com>
2017-04-16 09:23:05 -0400

    Options

--- app/guidebox.js
+++ app/guidebox.js
@@ -4,8 +4,12 @@ var client = require('guidebox')
 
 var Guidebox = new client('43f0aa6f1845fa3c37bee5c9dc58fd5d6b84731c')
 
+var options = {
+    limit: 2,
+    sources: 'free,netflix,hbo'
+}
 
-var shows = Guidebox.shows.list({limit: 2})
+var shows = Guidebox.shows.list(options)
 .then((response) => {
  console.log(response.results)
 }, (response) => {
